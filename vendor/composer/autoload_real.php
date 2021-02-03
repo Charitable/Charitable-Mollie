7092d9ee80dae3d8f29b621d6c0a3339097858f1
Eric Daams <eric@164a.com>
2021-02-03 10:48:43 +0930

    Updated composer packages. Tweaking Charitable packages to avoid redefining functions

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit8672133ad7c4b73b0cafac78212035cc
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit8672133ad7c4b73b0cafac78212035cc::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire8672133ad7c4b73b0cafac78212035cc($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire8672133ad7c4b73b0cafac78212035cc($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
